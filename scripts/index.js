89ee31520c2854cff129733e2c1b5f91a4fcb61c
github username <github email address>
2019-08-21 10:54:52 -0500

    namespace #1 complete

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,17 @@
 function main() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+try {
+  Item.validateName(name);
+  store.items.push(Item.create(name));
+} catch(error) {
+  console.log('Cannot add item: ' + error.message);
+}
+});
+shoppingList.render();
 }
 
 $(main);
